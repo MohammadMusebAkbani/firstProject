fb7fa17fab7a2758687e92ce00eed748f346cfc6
MohammadMusebAkbani <akbanimuseb@gmail.com>
2025-06-23 13:03:53 +0530

    Created Header Component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function HeroSection() {
+  return (
+    <main className="hero">
+      <div className="hero-content">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+        </p>
+      </div>
+      <div className="hero-image"></div>
+    </main>
+  );
+}
+
+export default HeroSection;
