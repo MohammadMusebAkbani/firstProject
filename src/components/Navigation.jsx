8d356aa71553e5d3eb6ce5c8a17c12da7c3e8555
MohammadMusebAkbani <akbanimuseb@gmail.com>
2025-06-23 14:50:08 +0530

    Shoe Page Created

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import "../App.css";
 
 export default function Navigation() {
   return (
-     <nav>
+     <nav className='container'>
           <div className="logo">
             <img src="/images/brand_logo.png" alt="logo"/>
           </div>
